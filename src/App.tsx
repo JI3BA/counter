0b150c8dfdd8a42332fa3137a83be94fb83e7637
JI3BA <lyovkin_artyom@mail.ru>
2023-07-31 20:31:26 +0300

    Added second counter to change it on redux functional and fixed some styles, fix markup to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ import {Counter} from "./components/Counter/Counter";
 function App() {
   return (
     <div className="App">
-      <Counter />
+        <div className='container'>
+            <div className='counter-container'>
+                <h1 className='counter__title'>Hooks</h1>
+                <Counter />
+            </div>
+
+            <div className='counter-container'>
+                <h1 className='counter__title'>Redux</h1>
+                <Counter />
+            </div>
+
+        </div>
     </div>
   );
 }
