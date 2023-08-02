b439f909521919ffee21d644edd6ed985a2f5852
JI3BA <lyovkin_artyom@mail.ru>
2023-08-02 21:54:48 +0300

    added new functional (changed min max value) and open setting window

--- src/components/Counter/CounterRedux.tsx
+++ src/components/Counter/CounterRedux.tsx
@@ -2,12 +2,25 @@ import './Counter.css'
 import {Button} from "../Button/Button";
 import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks";
 import {counterSlice} from "../../store/reducers/counterSlice";
+import {Input} from "../Input/Input";
+import {ChangeEvent} from "react";
 
 export const CounterRedux = () => {
     const count = useAppSelector(state => state.counterSlice.counter)
     const isSettings = useAppSelector(state => state.counterSlice.isSettings)
+    const maxValue = useAppSelector(state => state.counterSlice.maxValue)
+    const minValue = useAppSelector(state => state.counterSlice.minValue)
     const dispatch = useAppDispatch()
-    const { increment, reset } = counterSlice.actions
+    const {
+        increment,
+        reset,
+        isSetting,
+        increaseMaxValue,
+        decreaseMaxValue,
+        changeMaxValue,
+        changeMinValue,
+        decreaseMinValue,
+        increaseMinValue } = counterSlice.actions
 
     return(
         <div className='counter'>
@@ -16,7 +29,19 @@ export const CounterRedux = () => {
                     ? <span className="screen__count">{count}</span>
                     :
                     <div className='settings'>
-                        hi
+                        <div className="setting">
+                            <span className='setting__name'>Max value</span>
+                            <Button className="setting__button" disabled={Number(maxValue) < 1} onClick={() => dispatch(decreaseMaxValue())}>-</Button>
+                            <Input className='setting__input' type='number' placeholder='Max' value={maxValue.toString()} onChange={(e: ChangeEvent<HTMLInputElement>) => dispatch(changeMaxValue(e.target.value))} />
+                            <Button className="setting__button" onClick={() => dispatch(increaseMaxValue())}>+</Button>
+                        </div>
+
+                        <div className="setting">
+                            <span className='setting__name'>Start value</span>
+                            <Button className="setting__button" disabled={minValue < 1} onClick={() => dispatch(decreaseMinValue())}>-</Button>
+                            <Input className='setting__input' type='number' placeholder='Start' value={minValue.toString()} onChange={(e: ChangeEvent<HTMLInputElement>) => dispatch(changeMinValue(e.target.value))} />
+                            <Button className="setting__button" onClick={() => dispatch(increaseMinValue())}>+</Button>
+                        </div>
 
                     </div>
                 }
@@ -27,11 +52,11 @@ export const CounterRedux = () => {
                 {!isSettings
                     ?
                     <>
-                        <Button className="button"   onClick={() => dispatch(increment())}>inc</Button>
-                        <Button className="button"  onClick={() => dispatch(reset())}>reset</Button>
-                        <Button className="button" onClick={() => console.log('isSet')}>settings</Button>
+                        <Button className="button" disabled={count === maxValue} onClick={() => dispatch(increment())}>inc</Button>
+                        <Button className="button" disabled={count > minValue}onClick={() => dispatch(reset())}>reset</Button>
+                        <Button className="button" onClick={() => dispatch(isSetting())}>settings</Button>
                     </>
-                    : <Button className="button" onClick={() => console.log('set')}>set</Button>
+                    : <Button className="button" onClick={() => dispatch(isSetting())}>set</Button>
                 }
             </div>
         </div>
