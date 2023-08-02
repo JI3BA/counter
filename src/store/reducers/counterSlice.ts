5eb274424c1e2dc9359c78fd434114556ed83ddf
JI3BA <lyovkin_artyom@mail.ru>
2023-08-02 21:53:23 +0300

    added new reducers for min max value and settings

--- src/store/reducers/counterSlice.ts
+++ src/store/reducers/counterSlice.ts
@@ -1,25 +1,50 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-interface CartState {
+interface CounterState {
     counter: number,
-    isSettings: boolean
+    isSettings: boolean,
+    minValue: number,
+    maxValue: number,
 }
 
-const initialState: CartState = {
+const initialState: CounterState = {
     counter: 0,
-    isSettings: false
+    isSettings: false,
+    minValue: 0,
+    maxValue: 0
 }
 
 export const counterSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        increment: (state) => {
+        increment: state => {
             state.counter += 1
         },
-        reset: (state) => {
+        reset: state => {
             state.counter = 0
         },
+        isSetting: state => {
+            state.isSettings = !state.isSettings
+        },
+        increaseMinValue: state => {
+            state.minValue += 1
+        },
+        decreaseMinValue: state => {
+            state.minValue -= 1
+        },
+        increaseMaxValue: state => {
+            state.maxValue += 1
+        },
+        decreaseMaxValue: state => {
+            state.maxValue += 1
+        },
+        changeMinValue: (state, action: PayloadAction<string>) => {
+            state.minValue = Number(action.payload)
+        },
+        changeMaxValue: (state, action: PayloadAction<string>) => {
+            state.maxValue = Number(action.payload)
+        }
     }
 })
 
