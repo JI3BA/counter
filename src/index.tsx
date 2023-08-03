c2f6bfc7c4103e4f6dcafe32a1111ecedf381b74
JI3BA <lyovkin_artyom@mail.ru>
2023-08-03 20:49:36 +0300

    added Provider, PersistGate for keep data

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+import {setupStore} from "./store";
+
+const { store, persistor } = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
 );
 
