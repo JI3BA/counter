3ef289d5fe88d70831e678bd2de7eff4ae40d088
JI3BA <lyovkin_artyom@mail.ru>
2023-08-03 20:52:23 +0300

    fixed store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,44 +1,36 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import counterSlice from "./reducers/counterSlice";
-import { persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
 
 const rootReducer = combineReducers({
-   counter: counterSlice
-})
+    counter: counterSlice
+});
 
 export const setupStore = () => {
-    return configureStore({
-        reducer: rootReducer
-    })
-}
+    const persistConfig = {
+        key: 'root',
+        storage,
+    };
 
-const persistConfig = {
-    key: 'root',
-    storage,
-}
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+    const store = configureStore({
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                },
+            }),
+    });
 
-const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-})
+    const persistor = persistStore(store);
 
-export const persistor = persistStore(store)
+    return { store, persistor };
+};
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['store']['dispatch'];
