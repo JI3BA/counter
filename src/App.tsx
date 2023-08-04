cb8c349766b6036c285afb5af54f287edbc1a076
JI3BA <lyovkin_artyom@mail.ru>
2023-08-04 11:03:44 +0300

    took the provider up a notch to index

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,21 @@ import React from 'react';
 import './App.css';
 import {Counter} from "./components/Counter/Counter";
 import {CounterRedux} from "./components/Counter/CounterRedux";
-import {Provider} from "react-redux";
-import {setupStore} from "./store";
-
-const store = setupStore()
 
 export const App = () => {
   return (
-    <div className="App">
-        <div className='container'>
-            <div className='counter-container'>
-                <h1 className='counter__title'>Hooks</h1>
-                <Counter />
-            </div>
+      <div className="App">
+          <div className='container'>
+              <div className='counter-container'>
+                  <h1 className='counter__title'>Hooks</h1>
+                  <Counter />
+              </div>
 
-            <Provider store={store}>
-                <div className='counter-container'>
-                    <h1 className='counter__title'>Redux</h1>
-                    <CounterRedux />
-                </div>
-            </Provider>
-
-        </div>
-    </div>
+              <div className='counter-container'>
+                  <h1 className='counter__title'>Redux</h1>
+                  <CounterRedux />
+              </div>
+          </div>
+      </div>
   );
 }
